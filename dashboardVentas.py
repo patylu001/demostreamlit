7e39d9e7d185460e9870d3563d385110817ab6cd
patylu001 <patricia.ortegon@gmail.com>
2025-03-12 19:11:56 -0600

    Update dashboardVentas.py

--- dashboardVentas.py
+++ dashboardVentas.py
@@ -32,19 +32,25 @@ except FileNotFoundError:
 except Exception as e:
     st.error(f"Error al leer el archivo o generar la gráfica: {e}")
 with st.sidebar:
-  # Filtros
-  region_filter = st.selectbox("Selecciona una región:", df['Region'].unique())
-  state_filter = st.selectbox("Selecciona un estado:", df['State'].unique())
-
-
 # Aplica los filtros
-filtered_df = df[(df['Region'] == region_filter) & (df['State'] == state_filter)]
-# Gráfica de pastel
-fig = px.pie(filtered_df, names='Category', title='Distribución por Categoría')
-st.plotly_chart(fig)
-# Muestra el resultado (solo la primera fila si hay resultados)
-if not filtered_df.empty:
-    st.write("Resultado:")
-    st.dataframe(filtered_df) # Muestra la primera fila del dataframe filtrado
-else:
-    st.write("No se encontraron datos que coincidan con los filtros seleccionados.")
+    # Filtro para la columna "Region"
+    region_filter = st.selectbox("Selecciona una región:", df['Region'].unique())
+    df_filtered = df[df['Region'] == region_filter]
+
+    # Segundo filtro para la columna "State" basado en el filtro de "Region"
+    state_filter = st.selectbox("Selecciona un estado:", df_filtered['State'].unique())
+    df_filtered = df_filtered[df_filtered['State'] == state_filter]
+
+
+
+
+  # Muestra el DataFrame filtrado
+  st.dataframe(df_filtered)
+
+  # Gráfica de pastel para la columna "Category"
+  category_counts = df_filtered['Category'].value_counts()
+  fig_pie = px.pie(category_counts, 
+                     values=category_counts.values, 
+                     names=category_counts.index, 
+                     title='Distribución de Categorías')
+  st.plotly_chart(fig_pie)
