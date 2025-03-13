5364881f83958bf10edf012342421db8812b3ec1
patylu001 <patricia.ortegon@gmail.com>
2025-03-12 19:12:48 -0600

    Update dashboardVentas.py

--- dashboardVentas.py
+++ dashboardVentas.py
@@ -33,24 +33,24 @@ except Exception as e:
     st.error(f"Error al leer el archivo o generar la gráfica: {e}")
 with st.sidebar:
 # Aplica los filtros
-    # Filtro para la columna "Region"
-    region_filter = st.selectbox("Selecciona una región:", df['Region'].unique())
-    df_filtered = df[df['Region'] == region_filter]
+# Filtro para la columna "Region"
+  region_filter = st.selectbox("Selecciona una región:", df['Region'].unique())
+  df_filtered = df[df['Region'] == region_filter]
 
-    # Segundo filtro para la columna "State" basado en el filtro de "Region"
-    state_filter = st.selectbox("Selecciona un estado:", df_filtered['State'].unique())
-    df_filtered = df_filtered[df_filtered['State'] == state_filter]
-
-
+# Segundo filtro para la columna "State" basado en el filtro de "Region"
+  state_filter = st.selectbox("Selecciona un estado:", df_filtered['State'].unique())
+  df_filtered = df_filtered[df_filtered['State'] == state_filter]
 
 
-  # Muestra el DataFrame filtrado
-  st.dataframe(df_filtered)
 
-  # Gráfica de pastel para la columna "Category"
-  category_counts = df_filtered['Category'].value_counts()
-  fig_pie = px.pie(category_counts, 
+
+# Muestra el DataFrame filtrado
+st.dataframe(df_filtered)
+
+# Gráfica de pastel para la columna "Category"
+category_counts = df_filtered['Category'].value_counts()
+fig_pie = px.pie(category_counts, 
                      values=category_counts.values, 
                      names=category_counts.index, 
                      title='Distribución de Categorías')
-  st.plotly_chart(fig_pie)
+st.plotly_chart(fig_pie)
