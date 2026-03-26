21c215609a7ca26c855a2fcbef07e0982aa2c8da
patylu001 <patricia.ortegon@gmail.com>
2026-03-25 21:03:08 -0600

    Update dashboardVentas2026.py

--- dashboardVentas2026.py
+++ dashboardVentas2026.py
@@ -107,8 +107,23 @@ else:
     st.plotly_chart(fig_products, use_container_width=True)
 
  # --- Sales by State Map ---
+    state_abbreviations = {
+    'Alabama': 'AL', 'Alaska': 'AK', 'Arizona': 'AZ', 'Arkansas': 'AR', 'California': 'CA',
+    'Colorado': 'CO', 'Connecticut': 'CT', 'Delaware': 'DE', 'District of Columbia': 'DC',
+    'Florida': 'FL', 'Georgia': 'GA', 'Hawaii': 'HI', 'Idaho': 'ID', 'Illinois': 'IL',
+    'Indiana': 'IN', 'Iowa': 'IA', 'Kansas': 'KS', 'Kentucky': 'KY', 'Louisiana': 'LA',
+    'Maine': 'ME', 'Maryland': 'MD', 'Massachusetts': 'MA', 'Michigan': 'MI', 'Minnesota': 'MN',
+    'Mississippi': 'MS', 'Missouri': 'MO', 'Montana': 'MT', 'Nebraska': 'NE', 'Nevada': 'NV',
+    'New Hampshire': 'NH', 'New Jersey': 'NJ', 'New Mexico': 'NM', 'New York': 'NY',
+    'North Carolina': 'NC', 'North Dakota': 'ND', 'Ohio': 'OH', 'Oklahoma': 'OK', 'Oregon': 'OR',
+    'Pennsylvania': 'PA', 'Rhode Island': 'RI', 'South Carolina': 'SC', 'South Dakota': 'SD',
+    'Tennessee': 'TN', 'Texas': 'TX', 'Utah': 'UT', 'Vermont': 'VT', 'Virginia': 'VA',
+    'Washington': 'WA', 'West Virginia': 'WV', 'Wisconsin': 'WI', 'Wyoming': 'WY'
+    }
+    
     st.subheader('Ventas por Estado (USA)')
     sales_by_state = filtered_df.groupby('State')['Sales'].sum().reset_index()
+    sales_by_state['State_Code'] = sales_by_state['State'].map(state_abbreviations)
     fig_state = px.choropleth(sales_by_state, 
                               locations='State', 
                               locationmode='USA-states', 
